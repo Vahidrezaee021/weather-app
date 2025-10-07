7ee68ffe5f7f1d45af516ab21051798fad141b46
vahidrezaee <vahidrezai@ymail.com>
2025-10-07 10:27:45 +0330

    search icon and button

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import requests
 import pytz
 
 
+def get_weather():
+    pass
+    
 
 root = tk.Tk()
 root.title("Weather App")
@@ -16,5 +19,14 @@ root.resizable(False, False)
 search_image = tk.PhotoImage(file="search.png")
 search_image_lable = tk.Label(root, image=search_image)
 search_image_lable.pack(pady=20, side=tk.TOP)
+
+
+textfield = tk.Entry(root, justify="center", width=17, font=("poppins", 25, "bold"), bg="#404040", fg="white", border=0)
+textfield.place(x=280, y=40)
+
+search_icon = tk.PhotoImage(file="search_icon.png")
+search_icon_button = tk.Button(root, image=search_icon, border=0, cursor="hand2", bg="#404040", command=get_weather)
+search_icon_button.place(x=590, y=34)
+
 
 root.mainloop()

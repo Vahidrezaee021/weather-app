0bc149b6f7700c6f48a4da60284d0818a18ff67c
vahidrezaee <vahidrezai@ymail.com>
2025-10-07 10:56:12 +0330

    lables

--- main.py
+++ main.py
@@ -47,4 +47,31 @@ time_lable.place(x=120, y=230)
 clock = tk.Label(root, font=("Helvetica", 20), fg="#4b4bcc")
 clock.place(x=120, y=270)
 
+label1 = tk.Label(root, text="WIND", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label1.place(x=120, y=400)
+
+label2 = tk.Label(root, text="HUMIDITY", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label2.place(x=280, y=400)
+
+label3 = tk.Label(root, text="DESCRIPTION", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label3.place(x=450, y=400)
+
+label4 = tk.Label(root, text="PRESSURE", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label4.place(x=670, y=400)
+
+
+temp_label = tk.Label(root, font=("arial", 70, "bold"), fg="#e355cd")
+temp_label.place(x=590, y=170)
+condition_lable = tk.Label(root, font=("arial", 15, "bold"), fg="#4b4bcc")
+condition_lable.place(x=590, y=270)
+
+wind_label = tk.Label(root, text="...", font=("arial", 20, "bold"), bg="#1ab5ef", fg="#404040")
+wind_label.place(x=120, y=430)
+humidity_label = tk.Label(root, text="...", font=("arial", 20, "bold"), bg="#1ab5ef", fg="#404040")
+humidity_label.place(x=280, y=430)
+description_label = tk.Label(root, text="...", font=("arial", 20, "bold"), bg="#1ab5ef", fg="#404040")
+description_label.place(x=450, y=430)
+pressure_label = tk.Label(root, text="...", font=("arial", 20, "bold"), bg="#1ab5ef", fg="#404040")
+pressure_label.place(x=670, y=430)
+
 root.mainloop()

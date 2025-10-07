f2c5fe645cdbfd9f050a8cbd2ab504a65205f97f
vahidrezaee <vahidrezai@ymail.com>
2025-10-07 10:38:59 +0330

    city/time/clock

--- main.py
+++ main.py
@@ -29,4 +29,22 @@ search_icon_button = tk.Button(root, image=search_icon, border=0, cursor="hand2"
 search_icon_button.place(x=590, y=34)
 
 
+logo_image = tk.PhotoImage(file="logo.png")
+logo_label = tk.Label(root, image=logo_image)
+logo_label.pack(side=tk.TOP)
+
+frame_image = tk.PhotoImage(file="box.png")
+frame_label = tk.Label(root, image=frame_image)
+frame_label.pack(pady=10, side=tk.TOP)
+
+
+city_lable = tk.Label(root, font=("arial", 40, "bold"), fg="#e355cd")
+city_lable.place(x=120, y=160)
+
+time_lable = tk.Label(root, font=("arial", 20, "bold"), fg="#4b4bcc")
+time_lable.place(x=120, y=230)
+
+clock = tk.Label(root, font=("Helvetica", 20), fg="#4b4bcc")
+clock.place(x=120, y=270)
+
 root.mainloop()

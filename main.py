bf752f6229b86830c5ec559cc864fd3a1843b936
vahidrezaee <vahidrezai@ymail.com>
2025-10-07 10:11:28 +0330

    searchbox

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False, False)
 
+search_image = tk.PhotoImage(file="search.png")
+search_image_lable = tk.Label(root, image=search_image)
+search_image_lable.pack(pady=20, side=tk.TOP)
 
 root.mainloop()
